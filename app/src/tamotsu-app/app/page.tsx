497d8c7eb7a7d6bf36f3ef59f616f611d978998b
yufyaj <yk1994k@gmail.com>
2024-11-18 01:32:54 +0900

    :sparkles: チャット一覧画面への同線追加

--- app/src/tamotsu-app/app/page.tsx
+++ app/src/tamotsu-app/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <button onClick={() => router.push('/account/common/chooseType')}>選択画面</button>
+      <button onClick={() => router.push('/account/common/chooseType')}>選択画面</button>
+      <button onClick={() => router.push('/account/nutritionist/chat/list')}>チャット一覧画面</button>
       </div>
     </main> 
   );
