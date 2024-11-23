ace48c28a0f79721fa50de630793eb5ba82827c8
yufyaj <yk1994k@gmail.com>
2024-11-23 14:15:20 +0900

    :recycle: ユーザー制御も大体できてきた

--- app/src/tamotsu-app/middleware.ts
+++ app/src/tamotsu-app/middleware.ts
@@ -1,25 +1,66 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./lib/line/line";
 import getMyToken from "./lib/token/getMyToken";
+import { selectUser } from "./lib/db/user";
 
 export async function middleware(request: NextRequest) {
+  // セッショントークンの検証
   const token = await getMyToken(request)
-  if (!token || !token.accessToken) {
+  console.log("token=>", token)
+  if (!token?.sub) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  if (!await verifyToken(token.accessToken as string)) {
+  // LINEアクセストークンの検証
+  const lineToken = token.accessToken as string
+  if (!lineToken || !await verifyToken(lineToken)) {
+    // トークンが無効な場合はログアウト処理
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  if (request.nextUrl.pathname.startsWith('/user')) {
-    // TODO: ここはハンドリングする
+  const userData = await selectUser(token.sub)
+  const existsUser = !!userData
+  const isClient = userData?.role === 'user'
+  const isNutritionist = userData?.role === 'nutritionist'
+
+  // アカウントタイプに基づくアクセス制御
+  if (request.nextUrl.pathname.startsWith('/account/client')) {
+    controlUserPath(request, existsUser, isClient)
   }
-  if (request.nextUrl.pathname.startsWith('/nutritionist')) {
-    // TODO: ここはハンドリングする
+
+  if (request.nextUrl.pathname.startsWith('/account/nutritionist')) {
+    controlNutritionistPath(request, existsUser, isNutritionist)
   }
 }
 
 export const config = {
   matcher: '/account/:path*',
+}
+
+// ユーザー制御
+function controlUserPath(request: NextRequest, existsUser: boolean, isClient: boolean) {
+  // ユーザーが存在しない場合は、プロフィール登録画面への接続OK
+  if (request.nextUrl.pathname === '/account/client/profile') {
+    if (!existsUser) {
+      return
+    }
+  }
+
+  if (!isClient) {
+    return NextResponse.redirect(new URL('/account/nutritionist', request.url))
+  }
+}
+
+// 栄養士制御
+function controlNutritionistPath(request: NextRequest, existsUser: boolean, isNutritionist: boolean) {
+  // ユーザーが存在しない場合は、プロフィール登録画面への接続OK
+  if (request.nextUrl.pathname === '/account/nutritionist/profile') {
+    if (!existsUser) {
+      return
+    }
+  }
+
+  if (!isNutritionist) {
+    return NextResponse.redirect(new URL('/account/client', request.url))
+  }
 }
